ccc441e8eb7a27d1bb30b95c4d71927bed746fc3
oscar <46996403c@iespoblenou.org>
2015-11-27 03:21:53 +0100

    Modifico el main para agregar el icono y cambiar el titulo de la ventana

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,19 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    private static Stage scena;
     @Override
     public void start(Stage primaryStage) throws Exception{
+        scena = primaryStage;
+        scena.getIcons().add(new Image("file:src\\iconos\\iconoFinal.png"));
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Tiempo");
-        primaryStage.setScene(new Scene(root, 600, 550));
+        primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
